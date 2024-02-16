e972c2ca3333178607b7e510a5f75b2409355811
Arpit Kumar <arpitkumar4000@gmail.com>
2024-02-16 18:19:35 +0530

    🎨 [Refactor] opengraph

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,11 @@
+export default async function Image() {
+  return (
+    <div className="h-full w-full">
+      <div className="flex h-full w-full items-center justify-center">
+        <h1 className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text font-black text-transparent">
+          Portfolio
+        </h1>
+      </div>
+    </div>
+  );
+}

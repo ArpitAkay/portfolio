61d34135fadd5c7811b6710d8873c4e75ff0de33
Arpit Kumar <arpitkumar4000@gmail.com>
2024-02-18 01:57:19 +0530

    🚀 [Add] opengraph-image.tsx

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -12,41 +12,31 @@ export const contentType = "image/png";
 export default async function Image() {
   return new ImageResponse(
     (
-      //write the code for animate text gradient with all the color
-      <svg
-        width={size.width}
-        height={size.height}
-        viewBox={`0 0 ${size.width} ${size.height}`}
-        xmlns="http://www.w3.org/2000/svg"
+      <div
+        style={{
+          width: "1200px",
+          height: "630px",
+          backgroundColor: "#333",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        <rect
-          x="0"
-          y="0"
-          width={size.width}
-          height={size.height}
-          fill="url(#gradient)"
-        />
-        <defs>
-          <linearGradient id="gradient" x1="0" x2="0" y1="0" y2="1">
-            <stop offset="0%" stopColor="#f12711" />
-            <stop offset="25%" stopColor="#f5af19" />
-            <stop offset="50%" stopColor="#f12711" />
-            <stop offset="75%" stopColor="#f5af19" />
-            <stop offset="100%" stopColor="#f12711" />
-          </linearGradient>
-        </defs>
-        <text
-          x="50%"
-          y="50%"
-          dominantBaseline="middle"
-          textAnchor="middle"
-          fontSize="90"
-          fontWeight="bold"
-          fill="url(#gradient)"
+        <h1
+          style={{
+            background: "linear-gradient(45deg, #f3ec78, #af4261)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+            fontSize: "96px",
+            fontWeight: "bold",
+            textAlign: "center",
+            margin: 0,
+            padding: "200px 0",
+          }}
         >
           Portfolio
-        </text>
-      </svg>
+        </h1>
+      </div>
     ),
     {
       ...size,

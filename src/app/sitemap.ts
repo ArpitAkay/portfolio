06ec1a00b3de465c4dd86b1193a7c31fd5c052cc
Arpit Kumar <arpitkumar4000@gmail.com>
2024-02-22 15:41:43 +0530

    🎨 [Refactor] Favicon

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: `${process.env.NEXT_PUBLIC_DOMAIN}`,
-      lastModified: "2024-02-21T19:00:00.021Z",
+      lastModified: "2024-02-22T15:42:00.021Z",
       changeFrequency: "monthly",
       priority: 1,
     },

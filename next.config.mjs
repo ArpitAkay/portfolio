d9750a356bf92e22b4554e85a6f962cb86dd66cd
Arpit Kumar <arpitkumar4000@gmail.com>
2024-02-28 18:40:32 +0530

    📝 [Minor] trailingSlash

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    trailingSlash: true,
     images: {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: '**',
-                // port: '',
-                // pathname: '/random/**',
+                hostname: '**'
             },
         ],
     },

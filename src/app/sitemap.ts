539309986eab390883a61d1b6dac1e5d343c4ed3
Arpit Kumar <arpitkumar4000@gmail.com>
2024-03-05 12:48:05 +0530

    📝 [Minor] sitemap.xml

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,23 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 1,
     },
-    {
-      url: `${process.env.NEXT_PUBLIC_DOMAIN}linkedin`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.7,
-    },
-    {
-      url: `${process.env.NEXT_PUBLIC_DOMAIN}github`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.8,
-    },
-    {
-      url: `${process.env.NEXT_PUBLIC_DOMAIN}resume`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.9,
-    },
   ];
 }
